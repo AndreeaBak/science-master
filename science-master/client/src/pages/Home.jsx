f639bcf87a80b55b8de150b2ee1d390fdfddd908
AndreeaBak <bakandreea01@gmail.com>
2023-05-16 09:39:27 +0300

    search-bar added

--- science-master/client/src/pages/Home.jsx
+++ science-master/client/src/pages/Home.jsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 const Home = () => {
   const [posts, setPosts] = useState([])
+  const [searchQuery, setSearchQuery] = useState('')
+  const [filteredPosts, setFilteredPosts] = useState([])
 
   const cat = useLocation().search
 
@@ -19,6 +21,17 @@ const Home = () => {
     fetchData()
   }, [cat])
 
+  useEffect(() => {
+    const filtered = posts.filter(post =>
+      post.title.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+    setFilteredPosts(filtered)
+  }, [posts, searchQuery])
+
+  const handleSearch = event => {
+    setSearchQuery(event.target.value)
+  }
+
   const getText = (html) => {
     const doc = new DOMParser().parseFromString(html, "text/html")
     return doc.body.textContent
@@ -26,8 +39,16 @@ const Home = () => {
 
   return (
     <div className='home'>
+      <div className='search-bar'>
+        <input
+          type='text'
+          value={searchQuery}
+          onChange={handleSearch}
+          placeholder='Caută...'
+        />
+      </div>
       <div className="posts">
-        {posts.map((post)=>(
+        {(searchQuery ? filteredPosts : posts).map(post =>(
           <div className="post" key={post.id}>
             <div className="img">
               <img src={`./upload/${post.img}`} alt="" />
